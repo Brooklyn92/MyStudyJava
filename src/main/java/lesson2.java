166168b0f1260a07cbf630c623183670feb4dee9
Brooklyn92 <Tubanar173russia>
2022-01-26 11:06:51 +0400

    study lesson2

---
+++ src/main/java/lesson2.java
@@ -0,0 +1,24 @@
+public class lesson2 {
+    public static void main(String[] args) {
+        byte b1 = 12;
+        byte b2 = 0B1100;
+        byte b3 = 014;
+        byte b4 = 0xC;
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+
+
+        short s1 = 1300;
+        short s2 = 0B010100010100;
+        short s3 = 02424;
+        short s4 = 0x514;
+        System.out.println("====================================");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+
+    }
+}
