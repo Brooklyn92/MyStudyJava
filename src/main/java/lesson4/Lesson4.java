7861438d06f1b9592d69435d52addee77d481c53
Brooklyn92 <Tubanar173russia>
2022-02-02 17:06:14 +0400

    study lesson6

--- src/main/java/lesson4/Lesson4.java
+++ src/main/java/lesson4/Lesson4.java
@@ -10,15 +10,31 @@ public class Lesson4 {
         this.id = id;
         this.name = name;
     }
+
+    public double poplnennieScheta(double popolnenie) {
+        balance+=popolnenie;
+        System.out.println(balance);
+        return popolnenie;
+    }
+
+    public double snyatieScheta(double snyatie) {
+        balance-=snyatie;
+        System.out.println(balance);
+        return snyatie;
+    }
 }
 
 class Lesson4Test {
     public static void main(String[] args) {
-        Lesson4 lesson4 = new Lesson4(1,"Alex",12.12);
+       /* Lesson4 lesson4 = new Lesson4(1,"Alex",12.12);
         System.out.println(lesson4);
         System.out.println("Client info: " + lesson4.id + " " + lesson4.name + " " + lesson4.balance);
         Lesson4 lesson2 = lesson4;
         System.out.println(lesson2);
+        */
+        Lesson4 lesson1= new Lesson4(2,"Alex", 200.5);
+        lesson1.poplnennieScheta(200);
+        lesson1.snyatieScheta(100);
 
     }
 }
