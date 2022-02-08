5e4df894b30b61da88ca2b850c98983b30c7d9b8
Brooklyn92 <Tubanar173russia>
2022-02-08 17:56:49 +0400

    study lesson7 and lesson8

---
+++ src/main/java/lesson7/pac2/pac3/Package2.java
@@ -0,0 +1,9 @@
+package lesson7.pac2.pac3;
+
+import lesson7.pac2.pac3.Package3;
+
+public class Package2 extends Package3  {
+    public static void main(String[] args) {
+        Package3 package3 = new Package3();
+    }
+}
