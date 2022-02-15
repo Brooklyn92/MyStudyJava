4320045d5a92faf253d50a5077c6d570b5c8a022
Brooklyn92 <Tubanar173russia>
2022-02-15 15:24:10 +0400

    study lesson25

---
+++ src/main/java/lesson25/Test1.java
@@ -0,0 +1,54 @@
+package lesson25;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Employee emp1 = new Teacher();
+        Employee emp2 = new Driver();
+        Employee emp3 = new Doctor();
+        emp1.work();
+        emp2.work();
+        emp3.work();
+        Help_able h = new Driver();
+        h.help();
+
+    }
+}
+
+abstract class Employee {
+    void sleep() {
+        System.out.println("Employee sleeps");
+    }
+        abstract void work();
+    }
+
+
+class Teacher extends Employee {
+    @Override
+    void  work() {
+        System.out.println("Teacher works");
+    }
+}
+
+class Driver extends Employee implements Help_able {
+
+    @Override
+    public void help() {
+        System.out.println("Driver sleep");
+    }
+
+    @Override
+    void  work() {
+        System.out.println("Driver works");
+    }
+}
+
+class Doctor extends Employee {
+    @Override
+    void  work() {
+        System.out.println("Doctor works");
+    }
+}
+
+interface Help_able {
+    void help();
+}
