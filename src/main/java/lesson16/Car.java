8437ca76c5d967d851dd1b3cdcfe4e39f83a08de
Brooklyn92 <Tubanar173russia>
2022-03-01 18:03:03 +0400

    study lesson16

---
+++ src/main/java/lesson16/Car.java
@@ -0,0 +1,22 @@
+package lesson16;
+
+public class Car {
+    String color;
+    String engine;
+
+    public Car(String color, String engine) {
+        this.color = color;
+        this.engine = engine;
+    }
+    final int a =5;
+
+    public Car abc() {
+
+
+    }
+}
+
+
+ class TestCar {
+    Car car1 = new Car("red", "V8");
+ }
