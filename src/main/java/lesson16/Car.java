c62e1fdeebe8af264db9397c304687b31b663789
Brooklyn92 <Tubanar173russia>
2022-03-02 18:16:03 +0400

    study lesson16

--- src/main/java/lesson16/Car.java
+++ src/main/java/lesson16/Car.java
@@ -8,15 +8,18 @@ public class Car {
         this.color = color;
         this.engine = engine;
     }
-    final int a =5;
 
-    public Car abc() {
+    final int a = 5;
 
-
+    public Car abc(String cvet) {
+        Car c10 = new Car(cvet, "V4");
+        return c10;
     }
+
+    public static void main(String[] args) {
+        Car c = new Car("red", "V6");
+        Car c2 = c.abc("Black");
+        System.out.println(c.color);
+    }
+
 }
-
-
- class TestCar {
-    Car car1 = new Car("red", "V8");
- }
