1b18127c019b42d61ba20e22a2351569a865f18f
Brooklyn92 <Tubanar173russia>
2022-03-10 17:14:47 +0400

    study lesson16, part4

--- src/main/java/lesson16/Test6.java
+++ src/main/java/lesson16/Test6.java
@@ -7,5 +7,11 @@ public class Test6 {
         String s = "ПРИВЕТ";
         String s1 = s.toLowerCase(Locale.ROOT); // метод заменяет все буквы на маленькие.
         System.out.println(s1);
+        String s2 = "Привет";
+        String s3 = s2.toUpperCase(Locale.ROOT); // метод заменяет все буквы на заглавные
+        System.out.println(s3);
+
+        boolean b = s.contains("е");// проверяет,содердит ли символ или символы в искомой строке
+        System.out.println(b);
     }
 }
