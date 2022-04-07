6c3680b30a2d3838aac42e04f65fa09967a72e95
Brooklyn92 <Tubanar173russia>
2022-04-07 17:47:22 +0400

    study lesson17

---
+++ src/main/java/lesson17_stringBuilder/Test3.java
@@ -0,0 +1,10 @@
+package lesson17_stringBuilder;
+
+public class Test3 {
+    public static void main(String[] args) {
+        StringBuilder sb1 = new StringBuilder("Hello");
+        StringBuilder sb2 = new StringBuilder("Good bye");
+        String s1 = new String(sb1);
+        String s2 = new String(sb2);
+    }
+}
