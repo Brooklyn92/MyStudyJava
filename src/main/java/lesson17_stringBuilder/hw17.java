faa84006c69a65903211de3900359a67c2ade595
Brooklyn92 <Tubanar173russia>
2022-04-08 17:29:01 +0400

    study lesson17

---
+++ src/main/java/lesson17_stringBuilder/hw17.java
@@ -0,0 +1,18 @@
+package lesson17_stringBuilder;
+
+public class hw17 {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder1 = new StringBuilder("Привет");
+        StringBuilder stringBuilder2 = new StringBuilder("привет");
+        System.out.println(ravenctvo(stringBuilder1, stringBuilder2));
+
+    }
+
+
+    public static boolean ravenctvo(StringBuilder sb1, StringBuilder sb2) {
+
+        return sb1.toString().equals(sb2.toString());
+
+
+    }
+}
