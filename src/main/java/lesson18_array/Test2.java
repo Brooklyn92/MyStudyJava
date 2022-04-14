f036650dbb32484f06a5859b09db6380861b55ca
Brooklyn92 <Tubanar173russia>
2022-04-14 17:19:16 +0400

    study lesson18

---
+++ src/main/java/lesson18_array/Test2.java
@@ -0,0 +1,8 @@
+package lesson18_array;
+
+public class Test2 {
+    public static void main(String[] args) {
+        String[] array1 = new String[3];
+        int[][] array2 = new int[3][];// пустые скобки означают, что число элементов формируется динамически
+    }
+}
