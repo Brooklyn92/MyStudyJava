e0878e19814a3b725d4d3d48d5f0743b7c570ffe
Brooklyn92 <Tubanar173russia>
2022-04-15 17:56:31 +0400

    study lesson18 pt1

---
+++ src/main/java/lesson18_array/Test3.java
@@ -0,0 +1,11 @@
+package lesson18_array;
+
+public class Test3 {
+    public static void main(String[] args) {
+        int array1[] = {1,9,3,-8,0,5,4,1};
+
+        for (int i = 0; i<array1.length; i++){
+
+        }
+    }
+}
