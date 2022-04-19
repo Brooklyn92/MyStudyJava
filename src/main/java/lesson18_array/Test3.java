cedf8af5c37539456539b1c09d662d3ecdf87bfc
Brooklyn92 <Tubanar173russia>
2022-04-19 17:58:39 +0400

    study lesson18 pt2

--- src/main/java/lesson18_array/Test3.java
+++ src/main/java/lesson18_array/Test3.java
@@ -1,11 +1,25 @@
 package lesson18_array;
 
+import java.util.Arrays;
+
 public class Test3 {
     public static void main(String[] args) {
+
         int array1[] = {1,9,3,-8,0,5,4,1};
 
-        for (int i = 0; i<array1.length; i++){
-
+        for (int i = 0; i<array1.length; i++) {
+            System.out.print(array1[i] + " "); // 1 9 3 -8 0 5 4 1
         }
+        System.out.println();
+
+        Arrays.sort(array1); // сортировка массива от меньшего к большему
+
+        for (int i = 0; i<array1.length; i++) {
+            System.out.print(array1[i] + " "); // -8 0 1 1 3 4 5 9
+        }
+
+        System.out.println();
+        int index1 = Arrays.binarySearch(array1, -8); // выполняет поиск определённого значения в определённом массиве
+        System.out.println(index1);
     }
 }
---
+++ src/main/java/lesson18_array/Test5.java
@@ -0,0 +1,26 @@
+package lesson18_array;
+
+public class Test5 {
+    public static void main(String[] args) {
+        double array1[] = {1.05, -3.04, 8.0, 9.19, -3, 0};
+        maxMin(array1);
+        maxMin(new double[] {2.5, 1, 10, -98.4, 0});
+
+    }
+    public static void maxMin(double [] array) {
+        double max = array[0];
+        double min = array[0];
+
+        for (int i = 1; i<array.length; i++) {
+
+            if (array[i]>max) {
+                max = array[i];
+            }
+
+            if (array[i]<min) {
+                min = array[i];
+            }
+        }
+        System.out.println("Минимальный элемент массива: " + min + ", максимальный элемент массива: " + max);
+    }
+}
