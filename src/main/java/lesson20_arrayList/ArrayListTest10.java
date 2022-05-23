affbe2d5014b187bdeff10e57b6a9f61d7212ddf
Brooklyn92 <Tubanar173russia>
2022-05-23 11:50:52 +0400

    study method class ArrayList and Collection

---
+++ src/main/java/lesson20_arrayList/ArrayListTest10.java
@@ -0,0 +1,18 @@
+package lesson20_arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTest10 {
+    public static void main(String[] args) {
+        ArrayList al1 = new ArrayList<>();
+        al1.add("1");
+        al1.add("2");
+        al1.add("3");
+        al1.add("4");
+        al1.add("5");
+        for (Object s: al1) {
+            System.out.println("Number " + s + " and length " + ((String)s).length());
+        }
+    }
+}
