7d99d9fd2906d2bc205ca16cdc7425724f02950c
Brooklyn92 <Tubanar173russia>
2022-05-31 18:00:00 +0400

    add

---
+++ src/main/java/lesson21_garbageCollection/ternaryOperator/TernaryOperatorTest.java
@@ -0,0 +1,8 @@
+package lesson21_garbageCollection.ternaryOperator;
+
+public class TernaryOperatorTest {
+    public static void main(String[] args) {
+        int a = 4;
+        System.out.println(a<4?7:"Привет");
+    }
+}
