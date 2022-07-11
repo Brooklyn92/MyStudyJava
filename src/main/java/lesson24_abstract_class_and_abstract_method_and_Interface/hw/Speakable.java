f0478db7a91430edb55311468f7f5341114dd6df
Brooklyn92 <Tubanar173russia>
2022-07-11 17:14:35 +0400

    dev HW24

---
+++ src/main/java/lesson24_abstract_class_and_abstract_method_and_Interface/hw/Speakable.java
@@ -0,0 +1,7 @@
+package lesson24_abstract_class_and_abstract_method_and_Interface.hw;
+
+public interface Speakable {
+    default void speak(){
+        System.out.println("Somebody speak");
+    }
+}
