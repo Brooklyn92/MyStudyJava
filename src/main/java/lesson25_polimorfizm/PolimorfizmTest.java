afd9bba7551242395d7bc0823af52c1f6a2f4f6f
Brooklyn92 <Tubanar173russia>
2022-07-20 15:34:23 +0400

    study lesson25 pt1

--- src/main/java/lesson25_polimorfizm/PolimorfizmTest.java
+++ src/main/java/lesson25_polimorfizm/PolimorfizmTest.java
@@ -1,15 +1,19 @@
 package lesson25_polimorfizm;
 
-public class Test1 {
+public class PolimorfizmTest {
     public static void main(String[] args) {
         Employee emp1 = new Teacher();
         Employee emp2 = new Driver();
         Employee emp3 = new Doctor();
-        emp1.work();
-        emp2.work();
-        emp3.work();
-        Help_able h = new Driver();
-        h.help();
+//        emp1.work(); // -> Teacher works
+//        emp2.work(); // -> Driver works
+//        emp3.work(); // -> Doctor works
+//        Help_able h = new Driver();
+//        h.help(); // -> Driver sleep
+        Employee [] array1 = {emp1, emp2, emp3};
+        for(Employee employees: array1) {
+          employees.work(); // ->  Teacher works, Driver works, Doctor works
+        }
 
     }
 }
