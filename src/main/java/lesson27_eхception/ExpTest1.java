800855613f968c27158ba1f8fa62d97c5d913736
Brooklyn92 <Yurchenko221292@yandex.ru>
2022-07-28 00:05:39 +0400

    lesson27 exception study pt2

--- src/main/java/lesson27_eхception/ExpTest1.java
+++ src/main/java/lesson27_eхception/ExpTest1.java
@@ -3,9 +3,17 @@ package lesson27_eхception;
 import java.io.*;
 
 public class ExpTest1 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         File file = new File("D:\\Projects\\StudyJavaUdemy\\src\\test.txt");
-        FileOutputStream fileOutputStream = new FileOutputStream(file);
-        fileOutputStream.write(111);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(111);
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Файл для ввода текста не найден");
+        }
+        catch (IOException e) {
+            System.out.println("Запись файла не возможна");
+        }
     }
 }
