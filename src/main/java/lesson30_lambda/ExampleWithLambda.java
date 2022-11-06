29bc49335e08043e6138a2361cc49c06c813ed74
Brooklyn92 <Yurchenko221292@yandex.ru>
2022-11-06 16:01:44 +0400

    lesson30 pt2

--- src/main/java/lesson30_lambda/ExampleWithLambda.java
+++ src/main/java/lesson30_lambda/ExampleWithLambda.java
@@ -3,6 +3,31 @@ package lesson30_lambda;
 import java.util.ArrayList;
 
 public class ExampleWithLambda {
+    public static void main(String[] args) {
+        ArrayList<Student3> list = new ArrayList<>();
+        Student3 student1 = new Student3("Ivan", "male", 22, 3, 8.3);
+        Student3 student2 = new Student3("Nikolay", "male", 28, 2, 6.4);
+        Student3 student3 = new Student3("Elena", "female", 19, 1, 8.9);
+        Student3 student4 = new Student3("Petr", "male", 35, 4, 7);
+        Student3 student5 = new Student3("Mariya", "female", 23, 3, 9.1);
+        list.add(student1);
+        list.add(student2);
+        list.add(student3);
+        list.add(student4);
+        list.add(student5);
+        StudentInfo3 studentInfo3 = new StudentInfo3();
+        studentInfo3.testStudents(list, (Student3 st) -> {return st.avg > 8.5;}); // => первый вид лямбды-выражения
+        System.out.println("============================================================================================");
+        studentInfo3.testStudents(list,  st ->  st.avg < 9); // => второй вид лямбды-выражения(Более распространённый)
+        System.out.println("============================================================================================");
+        studentInfo3.testStudents(list, (Student3 st) -> {return st.age > 25;});
+        System.out.println("============================================================================================");
+        studentInfo3.testStudents(list, (Student3 st) -> {return st.age < 27;});
+        System.out.println("============================================================================================");
+        studentInfo3.testStudents(list, (Student3 st) -> {return st.gender=="male";});
+        System.out.println("============================================================================================");
+        studentInfo3.testStudents(list, (Student3 st) -> {return st.avg > 7.2 && st.age < 23 && st.gender == "female";});
+    }
 }
 
 class Student3 {
