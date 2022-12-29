70663d21689b1b8a4b952bfa5f7d1fe468d4e8d7
Brooklyn92 <Yurchenko221292@yandex.ru>
2022-12-29 20:04:47 +0400

    study test refactoring

---
+++ src/main/java/ParsingFileWithBinaryCode.java
@@ -0,0 +1,31 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
+import java.util.UUID;
+
+public class ParsingFileWithBinaryCode {
+    public static void main(String[] args) throws IOException {
+//        File gff = new File("src/main/resources/photo.jpg");
+////        byte[] f = Files.readAllBytes(Path.of("src/main/resources/Test2.pdf"));
+//       String f2 = f.toString();
+//        System.out.println(f2);
+
+//        try {
+            FileInputStream fis = new FileInputStream(new File("src/main/resources/TestPNG.png"));
+            String f = new String(Base64.getEncoder().encode(fis.readAllBytes()));
+            System.out.println(f);
+//            fis.close();
+//
+//            }
+//        catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+
+        System.out.println(UUID.randomUUID());
+    }
+}
+
