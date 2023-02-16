801b79052c5c8ae47b5c58c51b5a107ef9342eb5
Brooklyn92 <Yurchenko221292@yandex.ru>
2023-02-16 20:46:20 +0400

    study Generics

---
+++ src/main/java/udemy/lesson31_generics/example1/Example1.java
@@ -0,0 +1,16 @@
+package udemy.lesson31_generics.example1;
+
+
+public class Example1 {
+    public static void main(String[] args) {
+        Info<String> info = new Info<>("Привет");
+        System.out.println(info); // => Info{value=Привет}
+
+        String s = info.getValue();
+        System.out.println(s); // => Привет
+
+        Info<Integer> info2 = new Info<>(100);
+        System.out.println(info2); // => Info{value=100}
+
+    }
+}
---
+++ src/main/java/udemy/lesson31_generics/example1/Info.java
@@ -0,0 +1,20 @@
+package udemy.lesson31_generics.example1;
+
+public class Info <T> {
+    private T value;
+
+    public Info(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Info{" +
+                "value=" + value +
+                '}';
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
