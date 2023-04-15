50c4d6464b00b4101f5a645eae1026d0df82b3e8
Brooklyn92 <Yurchenko221292@yandex.ru>
2023-04-15 20:51:52 +0400

    study streams methods

---
+++ src/main/java/ConvertingStreamToStringInOneElement.java
@@ -0,0 +1,43 @@
+import udemy_advanced_java.streams.lesson2.Student;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConvertingStreamToStringInOneElement {
+    public static void main(String[] args) {
+        Student student1 = new Student("Ivan", 'm',22,3,8.3);
+        Student student2 = new Student("Nikolay", 'm',28,2,6.4);
+        Student student3 = new Student("Elena", 'f',19,1,8.9);
+        Student student4 = new Student("Petr", 'm',35,4,7);
+        Student student5 = new Student("Mariya", 'f',23,3,7.4);
+
+        List<Student> students = new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+        students.add(student5);
+
+//        List<Student> strings = students.stream().filter(el -> el.getName() == "Ivan").collect(Collectors.toList());
+//        String s = strings.stream().distinct().findFirst().get().getName();
+//        System.out.println(s);
+
+//        String name = students.stream()
+//                .filter(s -> s.getName().equals("Ivan"))
+//                .map(Student::getName)
+//                .findFirst()
+//                .orElse(null);
+//        System.out.println(name);
+
+//        String name = null;
+//        if (students.stream().anyMatch(s -> s.getName().equals("Ivan"))) {
+//            name = students.stream()
+//                    .filter(s -> s.getName().equals("Ivan"))
+//                    .map(Student::getName)
+//                    .findFirst()
+//                    .orElse(null);
+//        }
+//        System.out.println(name);
+
+    }
+}
---
+++ src/main/java/udemy_advanced_java/streams/lesson1/MethodMap2.java
@@ -0,0 +1,26 @@
+package udemy_advanced_java.streams.lesson1;
+
+import udemy.lesson30_lambda.I;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MethodMap2 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Go");
+        list.add("Python");
+        list.add("Ruby");
+
+//        List<Integer> list1 = list.stream().map(String::length).collect(Collectors.toList());
+//        System.out.println(list1); //  => [4, 2, 6, 4]
+
+//        list = list.stream().map(s -> s.toUpperCase(Locale.ROOT)).filter(s -> s.contains("P")).collect(Collectors.toList());
+//        System.out.println(list.stream().findFirst().get()); // PYTHON
+
+//        List<Integer> list1 = list.stream().map(s -> s.length() * 2).collect(Collectors.toList());
+//        System.out.println(list1); //  => [8, 4, 12, 8]
+    }
+}
