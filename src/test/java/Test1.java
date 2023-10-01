90be3fa84db58db3a8725d9b385d92983740e16a
Aleksandr <yurchenko221292@mail.ru>
2023-10-02 00:33:38 +0400

    add allure

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,7 +1,9 @@
+import io.qameta.allure.Link;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Test1 {
+    @Link("https://mail.ru/")
     @Test
     public void  myFirstTest() {
         Assertions.assertTrue(true, "Тест прошёл успешно");
